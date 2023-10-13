d8795ee80f2ce51c5b91597fa3fbaa2827d8da8d
MohsinSheikh1 <sheikhmohsin181@gmail.com>
2023-10-13 23:26:03 +0500

    Testing User Creation

--- controllers/userController.js
+++ controllers/userController.js
@@ -5,20 +5,27 @@ exports.getUser = async (req, res) => {
   const id = req.params.id;
   const user = await User.findOne({ id: id });
   if (user) {
-    res.send(user.apiKey);
+    res.status(200).json({
+      message: "User found",
+      apiKey: user.apiKey,
+    });
   } else {
-    res.send("User not found");
+    res.status(404).json({
+      message: "User not found",
+    });
   }
 };
 
 exports.createUser = async (req, res) => {
   // create user if user is created send api_key
-  const id = req.body.id;
-  const apiKey = req.body.apiKey;
-  const user = new User({
-    id: id,
-    apiKey: apiKey,
-  });
-  await user.save();
-  res.send(apiKey);
+  console.dir(req, { depth: null });
+
+  // const id = req.body.id;
+  // const apiKey = req.body.apiKey;
+  // const user = new User({
+  //   id: id,
+  //   apiKey: apiKey,
+  // });
+  // await user.save();
+  // res.send(apiKey);
 };
--- models/User.js
+++ models/User.js
@@ -3,7 +3,7 @@ const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
   id: String,
-  apiKey: String
+  apiKey: String,
 });
 
 module.exports = mongoose.model("User", UserSchema);
