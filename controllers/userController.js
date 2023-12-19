c62d5311e9a472a210b9711eb38ed0c320beaea2
MohsinSheikh1 <sheikhmohsin181@gmail.com>
2023-12-19 09:55:24 +0500

    Update user controller to handle various subscription events. (generated by blackbox.ai )

--- controllers/userController.js
+++ controllers/userController.js
@@ -104,22 +104,162 @@ exports.handleEvent = async function (req, res) {
     const type = req.body.type;
     if (type == "uninstall") {
       //code to remove user data
+      const account_id = req.body.data.account_id;
+      const id = req.body.data.user_id;
+
+      const user = await User.findOneAndDelete({ account_id, id });
+      if (user) {
+        logger.log(
+          "info",
+          `User ${id} on account ${account_id} uninstalled the app.`,
+          {
+            account_id: account_id,
+            user_id: id,
+            ip_address: "IP address not found",
+          }
+        );
+      }
     } else if (type == "install") {
       //code for install
     } else if (type == "app_subscription_changed") {
       //code for app subscription changed
+      const account_id = req.body.data.account_id;
+
+      const { plan_id, renewal_date } = req.body.data.subscription;
+      const account = Account.findOne({ account_id: account_id });
+
+      if (account) {
+        const exports_remaining = account.exports_remaining;
+        const prev_plan_id = account.plan_id;
+        let new_exports;
+
+        if (prev_plan_id === "1001") {
+          if (plan_id === "1002") {
+            new_exports = exports_remaining + 40;
+          } else if (plan_id === "1003") {
+            new_exports = exports_remaining + 70;
+          }
+        } else if (prev_plan_id === "1002") {
+          if (plan_id === "1001") {
+            new_exports =
+              exports_remaining - 40 < 0 ? 0 : exports_remaining - 40;
+          } else if (plan_id === "1003") {
+            new_exports = exports_remaining + 30;
+          }
+        } else if (prev_plan_id === "1003") {
+          if (plan_id === "1001") {
+            new_exports =
+              exports_remaining - 70 < 0 ? 0 : exports_remaining - 70;
+          } else if (plan_id === "1002") {
+            new_exports =
+              exports_remaining - 30 < 0 ? 0 : exports_remaining - 30;
+          }
+        }
+
+        account.set({
+          plan_id,
+          renewal_date,
+          exports_remaining: new_exports,
+        });
+
+        await account.save();
+
+        logger.log(
+          "info",
+          `User ${decoded.dat.user_id} on account ${account_id} changed subscription.`,
+          {
+            account_id: account_id,
+            user_id: decoded.dat.user_id,
+            ip_address: "IP address not found",
+          }
+        );
+      }
     } else if (type == "app_subscription_renewed") {
       //code for app subscription renewed
+      const account_id = req.body.data.account_id;
+      const { plan_id, renewal_date } = req.body.data.subscription;
+
+      const account = Account.findOne({ account_id: account_id });
+
+      if (account) {
+        if (plan_id == "1001") {
+          account.set({
+            plan_id,
+            renewal_date,
+            exports_remaining: 30,
+          });
+        } else if (plan_id == "1002") {
+          account.set({
+            plan_id,
+            renewal_date,
+            exports_remaining: 70,
+          });
+        } else if (plan_id == "1003") {
+          account.set({
+            plan_id,
+            renewal_date,
+            exports_remaining: 100,
+          });
+        }
+        await account.save();
+      }
     } else if (type == "app_subscription_cancelled_by_user") {
       //code for app subscription cancelled by user (paid period is not ended)
     } else if (type == "app_subscription_cancelled") {
       //code for app subscription cancelled (paid period is ended)
+      const account_id = req.body.data.account_id;
+      await Account.deleteOne({ account_id });
     } else if (type == "app_subscription_cancellation_revoked_by_user") {
       //code for app subscription cancelled revoked by user
     } else if (type == "app_trial_subscription_started") {
       //code for app subscription trial started
+      const account_id = req.body.data.account_id;
+      const { plan_id, renewal_date } = req.body.data.subscription;
+      const exports_remaining = 20;
+
+      const account = Account.findOne({ account_id: account_id });
+      if (!account) {
+        const account = new Account({
+          account_id,
+          plan_id,
+          renewal_date,
+          exports_remaining,
+        });
+        await account.save();
+      }
+
+      logger.log(
+        "info",
+        `User ${decoded.dat.user_id} on account ${account_id} started trial.`,
+        {
+          account_id: account_id,
+          user_id: decoded.dat.user_id,
+          ip_address: "IP address not found",
+        }
+      );
     } else if (type == "app_trial_subscription_ended") {
       //code for app subscription trial ended
+      const account_id = req.body.data.account_id;
+      const { plan_id, renewal_date } = req.body.data.subscription;
+      const exports_remaining = 0;
+      let accountData = await Account.findOneAndUpdate(
+        { account_id },
+        {
+          plan_id,
+          renewal_date,
+          exports_remaining,
+        }
+      );
+      if (!accountData) {
+        const account = new Account({
+          account_id,
+          plan_id,
+          renewal_date,
+          exports_remaining,
+        });
+        await account.save();
+      }
+      logger.log("info", `Trial expired for account ${account_id}`);
     } else if (type == "app_subscription_created") {
       //code for app subscription created (new)
 
@@ -135,13 +275,27 @@ exports.handleEvent = async function (req, res) {
       } else if (plan_id === "1002") {
         exports_remaining = 100;
       }
-      const account = new Account({
-        account_id,
-        plan_id,
-        renewal_date,
-        exports_remaining,
-      });
-      await account.save();
+
+      //if account already had a trial
+      const account = Account.findOne({ account_id, plan_id: "1004" });
+
+      if (account) {
+        account.set({
+          plan_id,
+          renewal_date,
+          exports_remaining,
+        });
+        await account.save();
+      } else {
+        //if there was no trial before
+        const account = new Account({
+          account_id,
+          plan_id,
+          renewal_date,
+          exports_remaining,
+        });
+        await account.save();
+      }
     }
   }
 };
